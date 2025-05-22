12f8612caeaac8c519940b71729517377e89ac91
쑤언 탕_Xuan Thang <thangdx@hivelab.com>
2025-05-22 02:50:42 -0700

    Responsive Header #1

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-// const btn = document.querySelector(".menu_btn");
-// const 
-// menu.addEventListener('click', () => {
-//   menu.classList.add('is_open')
-// })
+const menuBtn = document.querySelector(".menu_btn");
+const menu = document.querySelector('.header .content_right')
+const backBtn = document.querySelector('.icon_back')
+console.log(menu)
+menuBtn.addEventListener('click', () => {
+  menu.classList.add('is_open')
+})
+
+backBtn.addEventListener('click', () => {
+  menu.classList.remove('is_open')
+})
